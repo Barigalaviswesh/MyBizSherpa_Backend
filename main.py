49fb92b1f956e328b7aab908476052717356a7db
Barigala viswesh <85337456+Barigalaviswesh@users.noreply.github.com>
2025-10-28 17:58:58 +0530

    Fix CORS origin URL by removing trailing slash

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://my-biz-sherpa-frontend.vercel.app/"
+    "https://my-biz-sherpa-frontend.vercel.app"
 ]
 
 app.add_middleware(
